8ca30ec3673b622a7799cf01ac64c6e8685570bb
heliosalves <helios25@gmail.com>
2018-05-26 17:49:27 -0400

    removed unnecessary code

--- src/components/forms/login/LoginForm.js
+++ src/components/forms/login/LoginForm.js
@@ -9,41 +9,33 @@ const LoginForm = ({
   password,
   onChange,
   onSubmit,
-  loading,
-  error,
 }) => (
-  <div>
-    {!loading && (
-      <StyledForm onSubmit={onSubmit}>
+  <StyledForm onSubmit={onSubmit}>
+    <TextField
+      required
+      label='email'
+      value={email}
+      onChange={onChange}
+      name='email'
+      fullWidth
+      margin='normal'
+    />
 
-        <TextField
-          required
-          label='email'
-          value={email}
-          onChange={onChange}
-          name='email'
-          fullWidth
-          margin='normal'
-        />
+    <TextField
+      required
+      label='password'
+      type='password'
+      value={password}
+      onChange={onChange}
+      name='password'
+      fullWidth
+      margin='normal'
+    />
 
-        <TextField
-          required
-          label='password'
-          type='password'
-          value={password}
-          onChange={onChange}
-          name='password'
-          fullWidth
-          margin='normal'
-        />
-
-        <Button variant='raised' color='primary' fullWidth>
-          Sign In
-        </Button>
-      </StyledForm>
-    )}
-    {error && <p>There was an error, please try again later</p>}
-  </div>
+    <Button variant='raised' color='primary' fullWidth>
+      Sign In
+    </Button>
+  </StyledForm>
 );
 
 LoginForm.propTypes = {
@@ -51,8 +43,6 @@ LoginForm.propTypes = {
   password: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
-  loading: PropTypes.bool.isRequired,
-  error: PropTypes.string.isRequired,
 };
 
 export default LoginForm;

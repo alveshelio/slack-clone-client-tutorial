97dfc6db68142b3844d44ce3f7f512fa86c7a7ea
heliosalves <helios25@gmail.com>
2018-05-26 17:59:17 -0400

    added transition

--- src/components/shared/Notifier.js
+++ src/components/shared/Notifier.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import Proptypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Snackbar from '@material-ui/core/Snackbar';
-import IconButton from '@material-ui/core/IconButton';
+import { Snackbar, IconButton } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 
 const Notifier = ({ message, handleClose, open }) => (
